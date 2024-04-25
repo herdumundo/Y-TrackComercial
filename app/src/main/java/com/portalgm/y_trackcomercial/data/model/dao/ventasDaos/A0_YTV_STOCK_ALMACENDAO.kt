e8fdb32fbef5278b513dd1542362ab7bd7bab241
herdumundo <hernanjose86@hotmail.com>
2024-04-25 11:42:01 -0400

    Se llego hasta AnulacionFacturaScreen

---
+++ app/src/main/java/com/portalgm/y_trackcomercial/data/model/dao/ventasDaos/A0_YTV_STOCK_ALMACENDAO.kt
@@ -0,0 +1,27 @@
+package com.portalgm.y_trackcomercial.data.model.dao.ventasDaos
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Transaction
+import com.portalgm.y_trackcomercial.data.model.entities.ventas_entities.A0_YTV_STOCK_ALMACEN_Entity
+import com.portalgm.y_trackcomercial.data.model.models.ventas.DatosDetalleLotes
+import com.portalgm.y_trackcomercial.data.model.models.ventas.OrdenVentaCabItem
+
+@Dao
+interface A0_YTV_STOCK_ALMACENDAO {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    @Transaction
+    suspend fun insertAll(ytvVendor: List<A0_YTV_STOCK_ALMACEN_Entity>)
+
+    @Query("delete from A0_YTV_STOCK_ALMACEN")
+    suspend fun eliminarTodos()
+
+    @Query("SELECT COUNT(*) FROM A0_YTV_STOCK_ALMACEN")
+    fun getCount(): Int
+    @Query("SELECT   ItmsGrpCod ,ItmsGrpNam,ItemCode,BatchNum,WhsCode,Quantity FROM A0_YTV_STOCK_ALMACEN WHERE itemCode  IN (:itemCodes) and whscode='ESM003'")
+    suspend fun getDetalleLotes(itemCodes: List<String>): List<DatosDetalleLotes>
+
+}
